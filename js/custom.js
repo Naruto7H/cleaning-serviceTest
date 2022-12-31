a9740a72fda70ce725e084594bf52301d9f111ed
Suhail N <116971843+Naruto7H@users.noreply.github.com>
2022-12-31 14:32:06 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,44 @@
+
+  (function ($) {
+
+  "use strict";
+
+    // COUNTER NUMBERS
+    jQuery('.counter-thumb').appear(function() {
+      jQuery('.counter-number').countTo();
+    });
+
+    // CUSTOM LINK
+    $('.smoothscroll').click(function(){
+    var el = $(this).attr('href');
+    var elWrapped = $(el);
+    var header_height = $('.navbar').height();
+
+    scrollToDiv(elWrapped,header_height);
+    return false;
+
+    function scrollToDiv(element,navheight){
+      var offset = element.offset();
+      var offsetTop = offset.top;
+      var totalScroll = offsetTop-navheight;
+
+      $('body,html').animate({
+      scrollTop: totalScroll
+      }, 300);
+    }
+});
+
+  })(window.jQuery);
+
+
+//checkbox to radio
+  $("input:checkbox[name='ModeofContact']").on('click', function() {
+    var $box = $(this);
+    if ($box.is(":checked")) {
+      var group = "input:checkbox[name='ModeofContact']";
+      $(group).prop("checked", false);
+      $box.prop("checked", true);
+    } else {
+      $box.prop("checked", false);
+    }
+  });
